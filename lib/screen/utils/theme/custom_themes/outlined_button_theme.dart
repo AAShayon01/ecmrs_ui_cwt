f7dd209b4e483386acea2ec960f9e78f62d9ce36
shayon <shayon.excellitai@gmail.com>
2023-10-21 22:15:42 +0600

    complete outlineBorder theme data

--- lib/screen/utils/theme/custom_themes/outlined_button_theme.dart
+++ lib/screen/utils/theme/custom_themes/outlined_button_theme.dart
@@ -5,5 +5,28 @@ class SOutlineButtonTheme{
 
   ///light
 
+static final lightOutlinedButtonTheme=OutlinedButtonThemeData(
+  style:
+  OutlinedButton.styleFrom(
+      elevation: 0,foregroundColor: Colors.black,
+    side: const BorderSide(color: Colors.blue),
+    textStyle: const TextStyle(fontSize: 16,color: Colors.black,
+    fontWeight: FontWeight.w600),
+    padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 20),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14))
+  ),
+);
+static final DarkOutlineButtonTheme=OutlinedButtonThemeData(
+  style: OutlinedButton.styleFrom(
+    elevation: 0,
+    side:const  BorderSide(color: Colors.blueAccent),
+    padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 20),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+    foregroundColor: Colors.white,
+    textStyle: const TextStyle(fontSize: 16,color: Colors.white,fontWeight: FontWeight.w600),
+
+
+  )
+);
 
 }
