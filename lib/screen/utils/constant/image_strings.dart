e3f3b0141b9b14edd56e4068ff85163a21692759
Asif Afroj Shayon <asifa@excelitai.com>
2023-10-22 11:15:21 +0600

    need to resolved image_strings.dart

---
+++ lib/screen/utils/constant/image_strings.dart
@@ -0,0 +1,11 @@
+///in this class contains all thr App images in String formats
+
+Class SImages{
+
+  ///---App logo
+    static const String darkApplogo="";
+    static const String lightApplogo="";
+  ///---Social logos
+    static const google="";
+    static const facebook="";
+}
