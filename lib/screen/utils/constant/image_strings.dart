8e4c0aaa76a3b9fde03d30c49f0f031d663ade9b
Asif Afroj Shayon <asifa@excelitai.com>
2023-10-22 12:34:56 +0600

    completing Constant with asset image and font poppins and coolvetics

--- lib/screen/utils/constant/image_strings.dart
+++ lib/screen/utils/constant/image_strings.dart
@@ -1,11 +1,11 @@
 ///in this class contains all thr App images in String formats
 
-Class SImages{
+class SImages {
 
-  ///---App logo
-    static const String darkApplogo="";
-    static const String lightApplogo="";
+  ///---App logo----just add color on ,where to use
+    static const String darkApplogo="assets/logos/S-logo.png";
+    static const String lightApplogo="assets/logos/S-logo.png";
   ///---Social logos
-    static const google="";
-    static const facebook="";
+    static const google="assets/logos/Google-logo.png";
+    static const facebook="assets/logos/facebook-logo.png";
 }
---
+++ lib/screen/utils/constant/text_strings.dart
@@ -0,0 +1,19 @@
+///this witll contains all the text that use in app in String format
+
+class STexts {
+  ///-Global Texts
+
+  ///-OnBoarding Texts
+  static const String onBoardingTitle1="Choose your Products";
+  static const String onBoardingTitle2="Select Payment Method";
+  static const String onBoardingTitle3="Deliver at your door step";
+
+  static const String onBoardingSubTitle1="Wellcome to a World of Limitless Choice -Your Perfet Products Awaits";
+  static const String onBoardingSubTitle2="For seamless Transactions,choose Your Payment Path -Your convenience ,our Priority!";
+  static const String onBoardingSubTitle3="From our Doorstep to yours -Swift,secure and Contactless Delivery";
+  ///-Home
+  static const String homeAppbartitle="Good day for Shopping";
+  static const String homeAppbarSubtitle="MD Asif Afroj Shayon";
+
+
+}
