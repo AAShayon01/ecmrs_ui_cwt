ec5114c74c44a1e41c92c9544897cd195a73e7b2
Asif Afroj Shayon <asifa@excelitai.com>
2023-10-23 09:41:01 +0600

    Validator is complete

--- lib/utils/validators/validation.dart
+++ lib/utils/validators/validation.dart
@@ -36,6 +36,11 @@ class SValidator{
       return 'Phone number required';
     }
 
+    final phoneRegExp=RegExp(r'^\d{10}$');
+    if(!phoneRegExp.hasMatch(value)){
+      return 'Invalid phone Number(10 digits phone number required)';
+    }
+     return null ;
   }
 
 }
